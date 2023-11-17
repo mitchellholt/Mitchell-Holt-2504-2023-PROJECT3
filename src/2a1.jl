ab155c1f5d3d1e16d9891d5fe87d3dca56c3d183
Mitchell Holt <mitchellholt2002@gmail.com>
2023-11-17 14:53:29 +1000

    Did the transformation stuff for FashionMNIST data

---
+++ src/2a1.jl
@@ -0,0 +1,74 @@
+using Pkg; Pkg.activate(".")
+
+using Plots, CSV, Tables
+
+const fashion_mnist_train_path = "data/FashionMNIST/fashion-mnist_train.csv"
+const fashion_mnist_rotations_train_path = begin
+    "data/FashionMNIST/fashion-mnist-rotated_train.csv"
+end
+const img_dims = 28
+
+const rotate_0_label = 0
+const rotate_90_label = 1
+const rotate_180_label = 2
+const rotate_270_label = 3
+
+
+# Identity function; indeed, the identity element of the monoid of functions of
+# type a -> a
+id(x) = x
+
+# compose a map with itself k times (in the monoid of functions a -> a).
+function compose(f :: Function, k :: Int)
+    k < 0 && error("Cannot calculate inverse of an arbitrary function")
+    return k == 0 ? id : f ∘ compose(f, k - 1)
+end
+
+
+# Rotate the matrix representation of the pixels in an image a quarter turn 
+# counter-clockwise.
+rotate_counter_clockwise(img) = mapslices(reverse, transpose(img); dims=1)
+
+
+# Display an image from the training set
+display_img(img) = heatmap(
+    img;
+    yflip = true, legend = false, c = cgrad([:black, :white]))
+
+
+# Read all the data in the input CSV file, apply rotations and then save it to
+# a CSV file
+function transform_data(input_file_name :: String, output_file_name :: String)
+    raw_data = Tables.matrix(
+        CSV.File(input_file_name; limit=1)) # limit for debug, remove later
+    # ignore existing labels, we will use our own
+    imgs = [
+        transpose(reshape(row[2:end], img_dims, img_dims))
+        for row in eachrow(raw_data)
+    ]
+    transformed = [
+        (i, j)
+        for i in 0:3
+        for j in map(compose(rotate_counter_clockwise, 4 - i), imgs)
+    ]
+    # Plot example
+    display(plot(map(x -> display_img(x[2]), transformed[1:4])...))
+    # Save to csv
+    out_matrix = Matrix{Int}(undef, length(transformed), (28 * 28) + 1)
+    for k in 1:length(transformed)
+        out_matrix[k, 1] = transformed[k][1]
+        out_matrix[k, 2:end] = transformed[k][2]
+    end
+    CSV.write(output_file_name, Tables.table(out_matrix))
+end
+
+
+# Import transformed data from a given file
+function import_transformed_csv(input_file_name :: String)
+    raw_data = Tables.matrix(
+        CSV.File(input_file_name)) # limit for debug, remove later
+    return [
+        (row[1], reshape(row[2:end], img_dims, img_dims))
+        for row in eachrow(raw_data)
+    ]
+end
